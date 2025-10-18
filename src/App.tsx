3aacaf5b5f5440cc9286a8f23b83d11479cfe424
Vitaliy T <tarantasQuantumPlex@gmail.com>
2025-10-18 22:12:58 +0200

    Init commit v0.01

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="#" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="#" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>My Greatest App</h1>
+      <h2>The mega counter (v0.0.1)</h2>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more nothing
+      </p>
+    </>
+  )
+}
+
+export default App
